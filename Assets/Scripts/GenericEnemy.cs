f4eb67fbedf8693707b33d972d9efd7d21a1bb38
itamardag <itamar.dag@huji.ac.il>
2019-10-11 17:30:17 +0300

    generic enemy movement

--- Assets/Scripts/GenericEnemy.cs
+++ Assets/Scripts/GenericEnemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,25 @@ public class GenericEnemy : MovingObject
     protected override void OnBeat()
     {
         MoveType stepDir = _moveSet[_moveIndex];
-
+        Vector3Int step = makeStep(stepDir);
+        transform.Translate(GameEngine.Instance.Tilemap.CellToWorld(_myPosition + step) - GameEngine.Instance.Tilemap.CellToWorld(_myPosition));
+        _myPosition += step;
         _moveIndex = ++_moveIndex % _moveSet.Count;
+    }
+
+    private Vector3Int makeStep(MoveType stepDir)
+    {
+        switch (stepDir)
+        {
+            case (MoveType.Down):
+                return new Vector3Int(0, -1, 0);
+            case (MoveType.Left):
+                return new Vector3Int(-1, 0, 0);
+            case (MoveType.Right):
+                return new Vector3Int(1, 0, 0);
+            case (MoveType.Up):
+                return new Vector3Int(0, 1, 0);
+        }
+        return Vector3Int.zero;
     }
 }
