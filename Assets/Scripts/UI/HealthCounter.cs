75b8ec6423d043b6beecf5e07dd5892c853cdf62
Abbabon <netanel.amit@gmail.com>
2019-10-13 00:02:09 +0300

    fixing prefabs

--- Assets/Scripts/UI/HealthCounter.cs
+++ Assets/Scripts/UI/HealthCounter.cs
@@ -3,23 +3,23 @@
 //using System.Collections.Generic;
 //using UnityEngine;
 //using UnityEngine.UI;
-
+//
 //public class HealthCounter : MonoBehaviour
 //{
 //    private Image _image;
 //    [SerializeField] private Sprite fullSprite;
 //    [SerializeField] private Sprite emptySprite;
-    
+//    
 //    [SerializeField] private int _index;
-
+//
 //    private void Awake(){
 //        _image = GetComponent<Image>();
 //    }
-
+//
 //    void Start(){
 //        GameEngine.Instance.HealthChanged += OnHealthChanged;
 //    }
-
+//
 //    private void OnHealthChanged(int health){
 //         var newSprite = (health >= _index) ? fullSprite : emptySprite;
 //         if (newSprite != _image.sprite)
