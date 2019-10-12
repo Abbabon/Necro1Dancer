7c28c5335bdb024e284892c1e04a1785651f26df
Abbabon <netanel.amit@gmail.com>
2019-10-12 17:36:31 +0300

    bug fix

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -118,7 +118,7 @@ public class PlayerMovementController : MovingObject
     public void HandleDrowning()
     {
         var floor = GameEngine.Instance.Tilemap.GetTile(GameEngine.Instance.Tilemap.WorldToCell(transform.position) + new Vector3Int(0, -1, 0));
-        if (floor.name.Equals("water") || floor.name.Equals("water_alt"))
+        if (floor != null && floor.name.Equals("water") || floor.name.Equals("water_alt"))
         {
             _animator.SetTrigger("Drown");
             GameEngine.Instance.PlayerDrown(); //transform.position = GameEngine.Instance.PlayerDrown();
