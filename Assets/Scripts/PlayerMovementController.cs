34f87ec79e7e861c27c9bd1171c354fbcc86b9a5
Amir Blum <amirblum@gmail.com>
2019-10-12 21:41:12 +0300

    Level design

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -133,7 +133,7 @@ public class PlayerMovementController : MovingObject
     public void HandleDrowning()
     {
         var floor = GameEngine.Instance.Tilemap.GetTile(GameEngine.Instance.Tilemap.WorldToCell(transform.position) + new Vector3Int(0, -1, 0));
-        if (floor != null && (floor.name.Equals("water") || floor.name.Equals("water_alt")))
+        if (floor != null && (floor.name.Equals("water") || floor.name.Equals("water_alt") || floor.name.Equals("water_no_swap")))
         {
             _animator.SetTrigger("Drown");
             transform.position = GameEngine.Instance.PlayerDrown();
