494bca7ae1e1e82874bcc4b8d9ff20a8818049a3
itamardag <itamar.dag@huji.ac.il>
2019-10-12 11:27:15 +0300

    redFlash

--- Assets/Scripts/ScreenFlash.cs
+++ Assets/Scripts/ScreenFlash.cs
@@ -4,31 +4,23 @@ using UnityEngine;
 
 public class ScreenFlash : MonoBehaviour
 {
-    public int numFlashes = 2;
-    public float timeBetweenFlash = 0.1f;
+    private float alphaMax = 0.4f;
+    private float alphaDecrement = 0.02f;
+    private float alphaIncrement = 0.1f;
+    CanvasGroup flashLayer = GameEngine.Instance.redFlashOfDoom;
 
-
-    // public IEnumerator FlashInput()
-    //{
-    //    // save the InputField.textComponent color
-    //    [SerializeField] private Canvas canvas;
-
-    //    Color defaultColor = input.textComponent.color;
-
-    //    for (int i = 0; i < numFlashes; i++)
-    //    {
-    //        // if the current color is the default color - change it to the flash color
-    //        if (input.textComponent.color == defaultColor)
-    //        {
-    //            input.textComponent.color = flashColor;
-    //        }
-    //        else // otherwise change it back to the default color
-    //        {
-    //            input.textComponent.color = defaultColor;
-    //        }
-    //        yield return new WaitForSeconds(timeBetweenFlash);
-    //    }
-    //    Destroy(input.gameObject, 1); // magic door closes - remove object
-    //    yield return new WaitForSeconds(1);
-    //}
+    public IEnumerator Flash()
+    {
+        while (flashLayer.alpha < alphaMax)
+        {
+            flashLayer.alpha += alphaIncrement;
+            yield return new WaitForSeconds(0.01f);
+        }
+        while (flashLayer.alpha > 0)
+        {
+            flashLayer.alpha += alphaIncrement;
+            yield return new WaitForSeconds(0.01f);
+        }
+    yield return new WaitForSeconds(1);
+    }
 }
